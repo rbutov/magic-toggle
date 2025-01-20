4716efdf92724a9f8168a3a5d5ab2783c470bd60
Ruslan Butov <ruslan@butov.dev>
2025-01-19 18:28:58 -0800

    Initial Commit

---
+++ Magic Toggle/Magic_ToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_ToggleApp.swift
+//  Magic Toggle
+//
+//  Created by Ruslan Butov on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Magic_ToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Magic ToggleTests/Magic_ToggleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_ToggleTests.swift
+//  Magic ToggleTests
+//
+//  Created by Ruslan Butov on 1/19/25.
+//
+
+import Testing
+@testable import Magic_Toggle
+
+struct Magic_ToggleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
